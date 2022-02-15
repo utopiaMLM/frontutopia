0f551ae8da02067424b8def2d8faf63721390483
christian <99627273+utopiaMLM@users.noreply.github.com>
2022-02-14 21:41:07 -0500

    Cambios

--- src/app/constants/global.ts
+++ src/app/constants/global.ts
@@ -1,7 +1,7 @@
 export const global = {
   URLBASE_API: 'https://apiutopia.herokuapp.com/api',
   URLBASE_FRONT: 'https://front-utopia.herokuapp.com',
-  profileID : '620b0e6a3dbe900016941c32',
+  profileID : '620b1194f6691200168f746f',
 
   //URLBASE_API: 'http://localhost:3001/api',
   //URLBASE_FRONT: 'http://localhost:4200',
